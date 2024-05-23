93d91b60d637d5bc323eb5b4353db847af54d73e
Jahle <Jahle@ABLACKMAN>
2024-05-23 14:34:33 -0400

    Add project files.

---
+++ Lab3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Lab3
+    {
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word to check if it is a palindrome:");
+            string eye = Console.ReadLine();
+
+
+            if (TestPalindrome(eye))
+            {
+                Console.WriteLine($"{eye} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{eye} is not a palindrome.");
+            }
+
+            static bool TestPalindrome(string palindrome)
+            {
+                bool IsPalindrome = false;
+                for (int i = 0; i < palindrome.Length / 2; i++)
+                {
+                    if (!(palindrome[i] == palindrome[palindrome.Length - i - 1]))
+                    {
+                        return false;
+                    }
+
+
+                }
+                return true;
+            }
+        }
+    }
+}
